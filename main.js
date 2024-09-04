b329ee9f3a4f4e8e78f608ad0761933170142b47
uunreee <ksenwee01@gmail.com>
2024-09-04 17:26:28 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// 1
+
+// let admin;
+// let name;
+// name = 'Джон';
+// admin = name;
+// alert(admin);
+
+// 2
+
+// let city = prompt('Введите название города');
+// let year = prompt('Введите год образования');
+// let people = prompt ('Введите кол-во населения');
+// let agecity = 2024 - year;
+// alert (`Городу ${city} исполнилось ${agecity} лет с момента его образования. Население ${people} лет `);
+
+// 3
+
+// let r = prompt ('Радиус напишите');
+// let s = Math.PI * (r**2);
+// alert (`Плошадт круга с радиусом ${r} равна ${s}`);
+
+// 4 
+
+let num1 = +prompt ('первое число');
+let num2 = +prompt ('второе число');
+alert (`Сумма =  ${num1 + num2} , Разность = ${num1 - num2}, Частное = ${num1/num2} `);
